439be94d2034dffdfd589c26bf2ec88e5605e16b
Matthieu Guffroy <mattgu74@gmail.com>
2014-02-22 04:16:06 +0100

    Form + NFC pour le reload

--- src/com/mattgu/cash/ReloadActivity.java
+++ src/com/mattgu/cash/ReloadActivity.java
@@ -1,15 +1,94 @@
 package com.mattgu.cash;
 
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ReloadActivity extends Activity {
 
+	private NfcAdapter mNfcAdapter;
+	private EditText mFieldMail;
+	private EditText mFieldBadge;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_reload);
+		
+		mFieldBadge = (EditText) findViewById(R.id.badge);
+		mFieldMail = (EditText) findViewById(R.id.email);
+		
+		mNfcAdapter = NfcAdapter.getDefaultAdapter(getApplicationContext());
+		if(mNfcAdapter == null) {
+            Toast.makeText(this, "NFC n'est pas disponible sur cet équipement", Toast.LENGTH_LONG).show();
+		}
+		else if(!mNfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC est désactivé", Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	final protected Boolean identifierIsAvailable() {
+		return mNfcAdapter != null;
+	}
+	
+    private String ByteArrayToHexString(byte [] inarray) 
+    {
+    	int i, j, in;
+    	String [] hex = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+    	String out= "";
+
+    	for(j = 0 ; j < inarray.length ; ++j) 
+        {
+    		in = (int) inarray[j] & 0xff;
+    		i = (in >> 4) & 0x0f;
+    		out += hex[i];
+    		i = in & 0x0f;
+    		out += hex[i];
+        }
+    	return out;
+    }
+    
+	@Override
+    protected void onNewIntent(Intent intent) {
+    	// Lecture de l'id du tag nfc
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        final String id = ByteArrayToHexString(tag.getId());
+        Log.d("NFCREAD", "Tag: " + id);
+
+        mFieldBadge.setText(id);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+		if(identifierIsAvailable()) {
+			PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+	 
+			IntentFilter ndefFilter = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+	 
+			IntentFilter[] filters = {ndefFilter};
+	 
+			String[][] techs = {{Ndef.class.getName()}};
+	 
+			mNfcAdapter.enableForegroundDispatch(this, pendingIntent, filters, techs);
+		}
+    }
+    
+    @Override
+	protected void onPause() {
+    	super.onPause();
+		if(identifierIsAvailable())
+			mNfcAdapter.disableForegroundDispatch(this);
 	}
 
 }
